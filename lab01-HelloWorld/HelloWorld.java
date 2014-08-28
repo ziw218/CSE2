d0df9f6b41c7ec32087cf09c5f9c1ef258a76aad
ziw218 <ziw218@lehigh.edu>
2014-08-28 03:24:57 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Zihao Wang
+//Denmo 0
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+  public static void main(String[] arges) {
+      
+//  print the statement with box around it
+    System.out.println("#############################");
+    System.out.println("#############################");  
+    System.out.println("#                           #");  
+    System.out.println("#     Hello World 2014      #");   
+    System.out.println("#                           #");
+    System.out.println("#############################");  
+    System.out.println("#############################");  
+    System.out.println("test");
+    
+    //test/testtest
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
